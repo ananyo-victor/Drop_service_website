7e35a683bb62ea864cebcc6f4dea167059c78444
ananyo-victor <ananyomaitroan@gmail.com>
2023-04-24 20:46:32 +0530

    shipping updated

--- shipping.js
+++ shipping.js
@@ -1,12 +1,28 @@
 function Open_billing(){
     const Billing_form = document.getElementById('Billing_form');
-    Billing_form.classList.toggle('hidden');
+    Billing_form.classList.remove('hidden');
     const active = document.getElementById("Same");
-    active.classList.remove("bg-gray-700");
+    active.classList.remove("bg-gray-800");
+    const passive = document.getElementById("Different");
+    passive.classList.add("bg-gray-800");
 }
 function Activate(){
     const active = document.getElementById("Same");
-    active.classList.add("bg-gray-700");
+    active.classList.add("bg-gray-800");
     const Billing_form = document.getElementById('Billing_form');
-    Billing_form.classList.remove("bg-gray-700");
+    Billing_form.classList.add('hidden');
+    const passive = document.getElementById("Different");
+    passive.classList.remove("bg-gray-800");
+}
+// JavaScript code to handle radio input group
+const colorRadios = document.getElementsByName("Billing");
+
+for (let i = 0; i < colorRadios.length; i++) {
+  colorRadios[i].addEventListener("click", function() {
+    for (let j = 0; j < colorRadios.length; j++) {
+      if (colorRadios[j] !== this) {
+        colorRadios[j].checked = false;
+      }
+    }
+  });
 }
